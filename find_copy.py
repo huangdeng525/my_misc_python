bf3ff259ac2eb0ce0818b4c21be77ce4c55dd62e
huangdeng525 <winlj@sina.com>
2018-03-12 21:54:00 +0800

    init

---
+++ find_copy.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# find_copy.py
+# 
+#  find the euql file in filesystem
+# 
+#  Copyright (C) 2018-2018 huangdeng @ chengdu
+# 
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License version 3 as
+#  published by the Free Software Foundation.
