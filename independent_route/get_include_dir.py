f500b8e3f5640bd0a0616ce300a285cb3c923616
huangdeng <huangdeng@andisat.net>
2018-08-30 15:48:30 +0800

    new

---
+++ independent_route/get_include_dir.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# get_include_dir.py
+#
+# build my include dictionary
+#
+#  Copyright (C) 2018-2018 huangdeng @ chengdu
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License version 3 as
+#  published by the Free Software Foundation.
+#  2018.8.30 init
+__author__ = 'huangdeng'
+
+import os
+
+
+def get_include_dir(root, replace_with):
+    for cur_path, dirs, files in os.walk(root):
+        for file in files:
+            if '.h' in file:
+                include_path = cur_path.replace(root, replace_with)
+                print(include_path)
+                break
+
+
+if __name__ == '__main__':
+        get_include_dir(r'E:\github\dcop', r'..\..\..')
